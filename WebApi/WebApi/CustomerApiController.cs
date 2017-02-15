ef5243c1c55fde7ade6bef6672cb107a25663e48
triangle0105 <543443317@qq.com>
2017-02-15 16:41:35 +0800

    submit1

--- WebApi/WebApi/CustomerApiController.cs
+++ WebApi/WebApi/CustomerApiController.cs
@@ -3,15 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Text;
 using System.Web.Http;
+using System.Web.Script.Serialization;
 using WebApi.Models;
 
 namespace WebApi.WebApi
 {
     public class CustomerApiController : ApiController
     {
-        // GET api/coustomerapi
-        public object Get()
+        /// summary
+        /// -----同源策略,限制跨域访问
+        /// summary
+        //public IHttpActionResult Get()
+        //{
+        //    var cutomers = new List<CustomerModel>
+        //    {
+        //        new CustomerModel
+        //        {
+        //            Id = Guid.NewGuid(),
+        //            Name = "Customer1",
+        //            Type = "Type1"
+        //        },
+        //        new CustomerModel
+        //        {
+        //            Id = Guid.NewGuid(),
+        //            Name = "Customer2",
+        //            Type = "Type2"
+        //        },
+        //        new CustomerModel
+        //        {
+        //            Id = Guid.NewGuid(),
+        //            Name = "Customer3",
+        //            Type = "Type3"
+        //        }
+        //    };
+        //    return Json<IEnumerable<CustomerModel>>(cutomers);
+        //    //JavaScriptSerializer serializer = new JavaScriptSerializer();
+        //    //string content = string.Format("{0}({1})", callback, serializer.Serialize(contacts));
+        //    //return new HttpResponseMessage(HttpStatusCode.OK)
+        //    //{
+        //    //    Content = new StringContent(content, Encoding.UTF8, "text/javascript")
+        //    //};
+        //}
+
+        // JSONP
+        public HttpResponseMessage Get(string callback)
         {
             var cutomers = new List<CustomerModel>
             {
@@ -20,43 +57,15 @@ namespace WebApi.WebApi
                     Id = Guid.NewGuid(),
                     Name = "Customer1",
                     Type = "Type1"
-                },
-                new CustomerModel
-                {
-                    Id = Guid.NewGuid(),
-                    Name = "Customer2",
-                    Type = "Type2"
-                },
-                new CustomerModel
-                {
-                    Id = Guid.NewGuid(),
-                    Name = "Customer3",
-                    Type = "Type3"
                 }
             };
-
-            return cutomers;
+            var serializer = new JavaScriptSerializer();
+            string content = string.Format("{0}({1})", callback, serializer.Serialize(cutomers));
+            return new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new StringContent(content, Encoding.UTF8, "text/javascript")
+            };
         }
-
-        // GET api/coustomerapi/5
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/coustomerapi
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/coustomerapi/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/coustomerapi/5
-        public void Delete(int id)
-        {
-        }
+       
     }
 }
